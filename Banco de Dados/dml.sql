0b7592436eb3f4de35ae605b7cc433c8de13a2d7
GabrielAs15 <bielsantos200510@gmail.com>
2022-06-03 15:26:05 -0300

    segundo commit

--- Banco de Dados/dml.sql
+++ Banco de Dados/dml.sql
@@ -1,7 +1,12 @@
 --CS0:01 CONSULTAR LOGIN
 SELECT id_usuario       id,
        ds_email         email,
-FROM   tb_usuario
+FROM   tb_login,
 WHERE  ds_email       = 'admin@email.com.br'
 AND    ds_senha       = '1234'; 
 
+
+
+select * from tb_cadastro;
+
+select * from tb_pendencias;
